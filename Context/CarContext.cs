3440faecdde1f2eac830dcd76de9b6dd11efbd04
Lucas Eduardo da Silva <Lucas.silva@amcom.com.br>
2020-11-28 15:13:19 -0300

    Add files via upload

---
+++ Context/CarContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoIntegradorADS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegradorADS.Context
+{
+    public class CarContext:DbContext
+    {
+        public CarContext(DbContextOptions<CarContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
+
---
+++ Controllers/CarController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetoIntegradorADS.Context;
+using ProjetoIntegradorADS.Models;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegradorADS.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly CarContext _context;
+
+        public CarController(CarContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cars.ToListAsync());
+        }
+
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+            {
+                return View(new Car());
+            }
+            else
+            {
+                return View(_context.Cars.Find(id));
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("CarId,Modelo,CodigoCarro,Ano,Kilometragem,Vendedor,Caracteristica,Marca,Cor")] Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                if (car.CarId == 0)
+                {
+                    _context.Add(car);
+                }
+                else
+                {
+                    _context.Update(car);
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(car);
+        }
+
+
+        // GET: Employee/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var employee = await _context.Cars.FindAsync(id);
+            _context.Cars.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
